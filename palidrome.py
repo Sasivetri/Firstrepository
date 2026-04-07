926cd21de31826560067001218fcab90e33d2203
sasi <apple@apples-MacBook-Pro.local>
2026-04-07 10:24:45 +0530

    version4

--- palidrome.py
+++ palidrome.py
@@ -1,6 +1,6 @@
 #write a code to check given word is palindrome or not
 word = "madam"
-is_palindrome==word==word[::-1]
+Is_palindrome=word==word[::-1]
 print("Palindrome")
 
-#for a change
+#for a changessss
